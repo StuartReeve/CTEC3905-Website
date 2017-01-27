7c133258b8396bd7e8b93bd27893f39d804ee932
Stuart Reeve <stuart.reeve1@gmail.com>
2017-01-27 14:38:38 +0000

    -moved api call to its own handler calls so the ajax call code won't need rewriting for each api call type.

--- javascript/summonerSearch.js
+++ javascript/summonerSearch.js
@@ -7,36 +7,17 @@ var summonerSearch = (function() {
     var region = regionSelect.value;
 
     function lookupSummoner() {
-
-        queryRiotApi();
+        var summonerEndpoint = buildSummonerEndpoint(summonerName, region);
+        console.log(summonerEndpoint);
+        var riotHandler = riotApiHandler();
+        riotHandler.queryRiotApi(summonerEndpoint);
 
     }
 
     //Use the region and summoner name to build a url for the api
-    function buildApiUrl(summonerName, region) {
+    function buildSummonerEndpoint(summonerName, region) {
         summonerName = encodeURIComponent(summonerName);
         return 'https://' + region + '.api.pvp.net/api/lol/' + region + '/v1.4/summoner/by-name/' + summonerName;
-    }
-
-    function queryRiotApi() {
-
-        if (summonerName !== "") {
-            var apiUrl = buildApiUrl(summonerName, region);
-
-            $.ajax({
-                url: 'http://riot-api-wrapper.local/?riotUrl=' + apiUrl,
-                type: 'GET',
-                dataType: 'json',
-                data: {},
-                success: function (json) {
-                    console.log(json);
-                },
-                error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    console.log("Failed to get summoner data.");
-                }
-
-            });
-        }
     }
 
 
