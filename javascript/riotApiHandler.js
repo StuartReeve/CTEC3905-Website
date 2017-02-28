e46155c4dbcfa52db58176413b52bfea12402a36
Stuart Reeve <stuart.reeve1@gmail.com>
2017-02-28 15:52:25 +0000

    -Changed api handler to use hosted wrapper. -Added on hover cursor to match list items

--- javascript/riotApiHandler.js
+++ javascript/riotApiHandler.js
@@ -7,7 +7,7 @@ let riotApiHandler = (function() {
             //Call is made to a php wrapper for the riot api as calls can't be made in javascript due to CORS.
             //PHP code also allows for the api key to be hidden.
             $.ajax({
-                url: 'http://riot-api-wrapper.local/?riotUrl=' + apiEndpoint,
+                url: 'http://riot-api-wrapper.sreeve.tech?riotUrl=' + apiEndpoint,
                 type: 'GET',
                 dataType: 'json',
                 data: {},
